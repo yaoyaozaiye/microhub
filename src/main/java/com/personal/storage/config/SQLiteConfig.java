10352eb130731741f82506a7ec513482379d363f
netcaty <974123274@qq.com>
2025-02-20 18:19:32 +0800

    Update SQLiteConfig.java

--- src/main/java/com/personal/storage/config/SQLiteConfig.java
+++ src/main/java/com/personal/storage/config/SQLiteConfig.java
@@ -1,6 +1,7 @@
 package com.personal.storage.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
